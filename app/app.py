b0e84df19907b7e4dd4b65dcdd46150ee982f419
Victoria Grose <victoria.grose@gmail.com>
2019-07-27 02:13:46 -0500

    yikes

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 #import pickle
-# from sklearn.externals import joblib
+from sklearn.externals import joblib
 #from twitterscraper.kill import scrape_tweets
 
 # import sqlalchemy
@@ -18,7 +18,7 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__, static_url_path='/static')
 
-# model=joblib.load("tweet.pkl")
+model=joblib.load("tweet.pkl")
 
 @app.route("/")
 def index():
@@ -31,10 +31,10 @@ def my_form_post():
    input_text = text.upper()
    return input_text
 
-# @app.route("/classify", methods=['POST'])
-# def scrape():
-#     table=scrape_tweets(input_text)
-#     return redirect("index.html", data=)
+@app.route("/scrape", methods=['POST'])
+def scrape():
+    table=scrape_tweets(input_text)
+    return redirect("index.html", data=table)
 
 @app.route("/bubblecloud")
 def bubblecloud():
