621faec072773719b53cb6ebde7cc1c55ada61e4
Sneha Kumari <sneha.kumari@northwestern.edu>
2019-07-27 10:37:16 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -22,6 +22,7 @@ app = Flask(__name__, static_url_path='/static')
 # location = '/static'
 # fullpath = os.path.join(location, 'tweet.pkl')
 # model = joblib.load(fullpath)
+# model=joblib.load('tweet.pkl')
 
 
 @app.route("/",methods=['GET'])
